bf990dedeaa248ccab18395bf209d93e8694b178
karthik <k.arunachalam@ed.ac.uk>
2020-05-29 09:54:07 +0100

    reorganized files

--- Sensitivity_analysis/Lothian/langp095/inflow_gas_production.m
+++
@@ -1,7 +0,0 @@
-function production = inflow_gas_production(k,mu,Pav,Tav,Pwf,re,rw,Pc,Tc)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-rho = rho_mahmood(Pav,Tav,Pc,Tc);
-production = (k.*rho.*2.*pi.*(Pav-Pwf))./(mu.*log(re./rw));
-end
