cf14875144c1711eabe146cb808fa5c73cc6d00f
ARUNACHALAM Karthik <s1584407@login02.ecdf.ed.ac.uk>
2020-09-10 19:09:51 +0100

    ran simulation

--- Lang_vol_sweep_0_5/Fracs/Fracs40/Ads60/inflow_gas_production.m
+++
@@ -1,7 +0,0 @@
-function production = inflow_gas_production(k,mu,Pav,Pwf,re,rw)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-rho = rho_mahmood(Pav);
-production = (k.*rho.*2.*pi.*(Pav-Pwf))./(mu.*log(re./rw));
-end
